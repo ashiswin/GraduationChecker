de25503822aff83e529b40a9b8b3838d27617ec6
Isaac Ashwin <ashiswin@gmail.com>
2018-03-05 17:01:58 +0800

    Added new stuff to navbar

--- nav.php
+++ nav.php
@@ -3,12 +3,13 @@
 	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<a class="navbar-brand" href="/">GradCheck</a>
+	<a class="navbar-brand" href="/"><?php echo SITE_NAME; ?></a>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav">
-			<li id="navGradcheck" class="nav-item"><a href="gradcheck.php" class="nav-link">Check</a></li>
-			<li id="navManage" class="nav-item"><a href="manage.php" class="nav-link">Manage</a></li>
+			<li id="navGradcheck" class="nav-item"><a href="check.php" class="nav-link">Check</a></li>
+			<li id="navManage" class="nav-item"><a href="modules.php" class="nav-link">Modules</a></li>
+			<li id="navPillars" class="nav-item"><a href="pillars.php" class="nav-link">Pillars</a></li>
 		</ul>
 	</div>
 </nav>
