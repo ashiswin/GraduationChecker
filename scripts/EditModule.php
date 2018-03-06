6dac571f5804ec062997030d53ef89a1c11f5c38
Isaac Ashwin <ashiswin@gmail.com>
2018-03-06 20:05:10 +0800

    Added edit script

---
+++ scripts/EditModule.php
@@ -0,0 +1,15 @@
+<?php
+	require_once "utils/database.php";
+	require_once "connectors/ModuleConnector.php";
+	
+	$id = intval($_POST['id']);
+	$moduleCode = trim($_POST['moduleCode']);
+	$moduleName = trim($_POST['moduleName']);
+	$requiredGrades = trim($_POST['requiredGrades']);
+	
+	$ModuleConnector = new ModuleConnector($conn);
+	
+	$response["success"] = $ModuleConnector->update($moduleCode, $moduleName, $requiredGrades, $id);
+	
+	echo(json_encode($response));
+?>
